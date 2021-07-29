e362b4625821c4527abd7cb28cb621a826141d76
Alfredo Gomes Pereira <71193893+alfredo1995@users.noreply.github.com>
2021-07-29 17:00:26 -0300

    Update lampada.js

---
+++ lampada.js
@@ -0,0 +1,11 @@
+   <script>
+         function acendeLampada(){
+            document.getElementById("luz").src = "img/acessa.jpg"    ;
+         }
+         function apagarLampada(){
+            document.getElementById("luz").src = "img/apagada.jpg"    ;
+         }
+         function quebrarLampada(){
+            document.getElementById("luz").src = "img/quebrada.jpg"    ;
+         }
+     </script>
